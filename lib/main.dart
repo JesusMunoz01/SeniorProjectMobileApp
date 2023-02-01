e6de524c440939b01487b40bbd8786adebf5f0fa
felcha <felix.charles01@utrgv.edu>
2023-02-01 15:54:39 -0600

    Updated Sprint 1

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'screens/firstScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
