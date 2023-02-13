39249a1568e5ad3622b9bc4ee0334d6d4a4d6b32
JesusMunoz01 <Jesus.munoz01@utrgv.edu>
2023-02-12 21:00:39 -0600

    updated unit tests

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -20,12 +20,20 @@ void main() {
       test("basic path test", () async {
         final screenTwoTestText = find.byValueKey('secondScreenTest');
         final screenTwoButton = find.byValueKey("secondScreenButtonTest");
+        final screenTwoLoginBox = find.byValueKey("usernameText");
+        final screenTwoPasswordBox = find.byValueKey("passwordText");
+        final screenTwoLoginButton = find.byValueKey("loginButton");
+        final screenTwoLoginTextButton = find.byValueKey("loginTextButton");
         final mainScreenText = find.byValueKey("mainScreenText");
         final mainScreenBudgetText = find.byValueKey("budgetText");
         final mainScreenButton = find.byValueKey("mainScreenButton");
         final mainScreenButtonText = find.byValueKey("buttonText");
 
-        expect(await driver.getText(screenTwoTestText), "Test Screen");
+        expect(await driver.getText(screenTwoTestText), "Login");
+        expect(await driver.getText(screenTwoLoginBox), "Username");
+        expect(await driver.getText(screenTwoPasswordBox), "Password");
+        expect(await driver.getText(screenTwoLoginTextButton), "Login");
+        await driver.tap(screenTwoLoginButton);
         await driver.tap(screenTwoButton);
 
         expect(await driver.getText(mainScreenText), "Test Screen");
