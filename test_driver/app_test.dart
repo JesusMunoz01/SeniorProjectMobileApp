10b2ff42793955df7aef481982fee3a985bf557d
JesusMunoz01 <Jesus.munoz01@utrgv.edu>
2023-02-22 09:40:31 -0600

    sprintFive

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -19,11 +19,15 @@ void main() {
     group("Template", () {
       test("basic path test", () async {
         final screenTwoTestText = find.byValueKey('secondScreenTest');
-        final screenTwoButton = find.byValueKey("secondScreenButtonTest");
+        final createAccText = find.byValueKey("createAccText");
+        final createAccButton = find.byValueKey("createAccButton");
         final screenTwoLoginBox = find.byValueKey("usernameText");
         final screenTwoPasswordBox = find.byValueKey("passwordText");
         final screenTwoLoginButton = find.byValueKey("loginButton");
         final screenTwoLoginTextButton = find.byValueKey("loginTextButton");
+        final usernameField = find.byValueKey("usernameTextField");
+        final passwordField = find.byValueKey("passwordTextField");
+
         final mainScreenText = find.byValueKey("mainScreenText");
         final mainScreenBudgetText = find.byValueKey("budgetText");
         final mainScreenButton = find.byValueKey("mainScreenButton");
@@ -33,8 +37,14 @@ void main() {
         expect(await driver.getText(screenTwoLoginBox), "Username");
         expect(await driver.getText(screenTwoPasswordBox), "Password");
         expect(await driver.getText(screenTwoLoginTextButton), "Login");
+        expect(await driver.getText(createAccText), "Create an Account");
+
+        await driver.tap(usernameField);
+        await driver.enterText("TestAccount");
+        await driver.tap(passwordField);
+        await driver.enterText("TestPassword");
         await driver.tap(screenTwoLoginButton);
-        await driver.tap(screenTwoButton);
+        //await driver.tap(createAccButton);
 
         expect(await driver.getText(mainScreenText), "Recommended");
         expect(await driver.getText(mainScreenButtonText), "Test");
