f2224e187464dad0b47d0314bf7fd55e42fe1e44
felcha <felix.charles01@utrgv.edu>
2023-03-01 16:14:18 -0600

    sprint 6

---
+++ lib/screens/settingsScreen.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:sp_grocery_application/screens/createAccScreen.dart';
+import 'package:sp_grocery_application/screens/mainScreen.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'mainScreen.dart';
+
+class SettingsScreen extends StatefulWidget {
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  TextEditingController _username = TextEditingController();
+  TextEditingController _password = TextEditingController();
+  @override
+  FirebaseDatabase database = FirebaseDatabase.instance;
+  DatabaseReference myRef = FirebaseDatabase.instance.ref("profiles/username");
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(20, 200, 20, 0),
+              child: Text(
+                "Settings",
+                key: Key("settingsScreenTest"),
+                style: TextStyle(fontSize: 18, fontFamily: 'Montserrat'),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 80, 0, 0),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
+              child: TextField(
+                key: Key("usernameTextField"),
+                controller: _username,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  label: Text(
+                    'Username',
+                    key: Key("usernameText"),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
+              child: TextField(
+                key: Key("passwordTextField"),
+                obscureText: true,
+                controller: _password,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  label: Text(
+                    'Password',
+                    key: Key("passwordText"),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+                height: 50,
+                padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                child: ElevatedButton(
+                  key: Key("loginButton"),
+                  child: const Text(
+                    'Login',
+                    key: Key("loginTextButton"),
+                  ),
+                  onPressed: () async {
+                    final snapshot = await FirebaseDatabase.instance
+                        .ref("profiles/${_username.text}/username")
+                        .get();
+                    final passSnapshot = await FirebaseDatabase.instance
+                        .ref("profiles/${_username.text}/password")
+                        .get();
+                    if (_username.text == snapshot.value.toString() &&
+                        _password.text.length > 8 &&
+                        _password.text == passSnapshot.value.toString()) {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MainScreen()));
+                    }
+                  },
+                )),
+            Container(
+              padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => createAccScreen()));
+                },
+                child: Text(
+                  "Create an Account",
+                  style: TextStyle(color: Colors.blue),
+                  key: Key("createAccText"),
+                ),
+                key: Key("createAccButton"),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
