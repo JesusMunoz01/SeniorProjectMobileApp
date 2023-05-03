393e5a8cba0a4f8a6476939eb263a19a14c23f7b
Jesus <jesus.munoz01@utrgv.edu>
2023-05-02 23:13:43 -0500

    expansion list and meal template

--- lib/screens/settingsScreen.dart
+++ lib/screens/settingsScreen.dart
@@ -23,6 +23,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         actions: [
           
